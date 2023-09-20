7aba32cecab8cf9e3e2dd14f76cacd74f3bdced7
Harris Pagonis <hpagonis@hotmail.com>
2023-09-20 23:47:22 +0300

    Added timeout for candle activation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #define CANDLE_NUM 5
 #define SIG_PIN 2
+#define THRESHOLD 800
+#define TIMEOUT 10000
 
 struct Candle {
   uint8_t LDR_pin;
@@ -10,16 +12,17 @@ struct Candle {
 };
 
 Candle candle[CANDLE_NUM] = {
-  {.LDR_pin = A0, .LED_pin = 7, .threshold = 800, false},
-  {.LDR_pin = A1, .LED_pin = 6, .threshold = 800, false},
-  {.LDR_pin = A2, .LED_pin = 5, .threshold = 800, false},
-  {.LDR_pin = A3, .LED_pin = 4, .threshold = 800, false},
-  {.LDR_pin = A4, .LED_pin = 3, .threshold = 800, false},
+  {.LDR_pin = A0, .LED_pin = 7, .threshold = THRESHOLD, false},
+  {.LDR_pin = A1, .LED_pin = 6, .threshold = THRESHOLD, false},
+  {.LDR_pin = A2, .LED_pin = 5, .threshold = THRESHOLD, false},
+  {.LDR_pin = A3, .LED_pin = 4, .threshold = THRESHOLD, false},
+  {.LDR_pin = A4, .LED_pin = 3, .threshold = THRESHOLD, false},
 };
 
-uint8_t target_seq[5] = {0,1,2,3,4};
+uint8_t target_seq[5] = {2,0,3,4,1};
 uint8_t player_seq[5];
 uint8_t index = 0;
+unsigned long last_lit = 0;
 
 
 void setup() {
@@ -51,6 +54,7 @@ void loop() {
       candle[i].lit = true;
       player_seq[index] = i;
       index++;
+      last_lit = millis();
     }
   }
   if (index == 5) {
@@ -66,6 +70,14 @@ void loop() {
       digitalWrite(candle[i].LED_pin, LOW);
       candle[i].lit = false;
       delay(200);
+    }
+    index = 0;
+    return;
+  }
+  if (millis() > last_lit + TIMEOUT) {
+    for (int i=0; i < CANDLE_NUM; i++) {
+      digitalWrite(candle[i].LED_pin, LOW);
+      candle[i].lit = false;
     }
     index = 0;
   }
